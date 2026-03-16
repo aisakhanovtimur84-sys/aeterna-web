563a7848145471849ef9de65bcbc95512380d499
aisakhanovtimur84-sys <aisakhanovtimur84@gmail.com>
2026-03-16 22:51:00 +0500

    Add FileUploader component to Create page

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,8 +1,16 @@
+import FileUploader from "../components/FileUploader";
+
 export default function Create() {
+
   return (
     <div style={{ padding: 40 }}>
+
       <h1>Create Capsule</h1>
-      <p>This page will contain the capsule builder.</p>
+
+      <p>Add files to your capsule.</p>
+
+      <FileUploader />
+
     </div>
   );
 }
