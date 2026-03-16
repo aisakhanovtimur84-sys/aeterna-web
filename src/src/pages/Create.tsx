b77277023cae7bf7d86056156c213a13f03f701f
aisakhanovtimur84-sys <aisakhanovtimur84@gmail.com>
2026-03-16 20:39:11 +0500

    Add Create component for capsule builder page

---
+++ src/src/pages/Create.tsx
@@ -0,0 +1,8 @@
+export default function Create() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Create Capsule</h1>
+      <p>This page will contain the capsule builder.</p>
+    </div>
+  );
+}
