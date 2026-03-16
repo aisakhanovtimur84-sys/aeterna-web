c985d5d33e576e3688fdaf5b92131984b6fb5f13
aisakhanovtimur84-sys <aisakhanovtimur84@gmail.com>
2026-03-16 22:46:30 +0500

    Wrap RouterProvider with CapsuleProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { CapsuleProvider } from "./context/CapsuleContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <CapsuleProvider>
+      <RouterProvider router={router} />
+    </CapsuleProvider>
   </React.StrictMode>
 );
