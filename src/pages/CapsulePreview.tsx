318c538eee207e041ad56215e18276d1d8593571
aisakhanovtimur84-sys <aisakhanovtimur84@gmail.com>
2026-03-16 22:36:29 +0500

    Create CapsulePreview component for capsule review

---
+++ src/pages/CapsulePreview.tsx
@@ -0,0 +1,8 @@
+export default function CapsulePreview() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Capsule Preview</h1>
+      <p>Review capsule contents before sealing.</p>
+    </div>
+  );
+}
