9a4e50c6d22dd41e6fd7c3ac3c93c885edbd7da0
aisakhanovtimur84-sys <aisakhanovtimur84@gmail.com>
2026-03-16 22:38:52 +0500

    Add CapsulePreview route to the router

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Create from "./pages/Create";
+import CapsulePreview from "./pages/CapsulePreview";
 import CapsuleView from "./pages/CapsuleView";
 
 export const router = createBrowserRouter([
@@ -11,6 +13,10 @@ export const router = createBrowserRouter([
   {
     path: "/create",
     element: <Create />,
+  },
+  {
+    path: "/capsule/preview",
+    element: <CapsulePreview />,
   },
   {
     path: "/capsule/:id",
